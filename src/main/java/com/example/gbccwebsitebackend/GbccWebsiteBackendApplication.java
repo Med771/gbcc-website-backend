b9c6f8672b65478d720a68e44b8e4f238353634c
Med <mm7737333@gmail.com>
2026-02-21 14:04:33 +0300

    Init commit

---
+++ src/main/java/com/example/gbccwebsitebackend/GbccWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gbccwebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbccWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbccWebsiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gbccwebsitebackend/GbccWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gbccwebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbccWebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
